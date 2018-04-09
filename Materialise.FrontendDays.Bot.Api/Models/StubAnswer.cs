6b0ee48626923cfa355230ac4f5eb3eeeba1d082
misikdmitriy <misik.dmitriy@gmail.com>
2018-04-09 18:08:12 +0300

    remove unused model

--- Materialise.FrontendDays.Bot.Api/Models/StubAnswer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Materialise.FrontendDays.Bot.Api.Models
-{
-    public class StubAnswer
-    {
-        public int Id { get; set; }
-        public virtual Question Question { get; set; }
-        public int QuestionId { get; set; }
-    }
-}
